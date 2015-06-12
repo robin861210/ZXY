31fec44490ca886f4882dbc94b89ac5aa3764002
acewill <acewill@acewillmatoMacBook-Pro-2.local>
2015-06-12 13:40:00 +0800

    MFJ 创建的一个程序模板。（这个模板还未进行修改，只是为了测试github的可用性）稍后将进行修改。

---
+++ ZXY/ZXY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXY
+//
+//  Created by acewill on 15/6/12.
+//  Copyright (c) 2015年 MFJ_zxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
